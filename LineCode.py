669b058689a6dfb014be9b7d7953ff6ff4d3e3ed
Andrea Oquendo <oquendo.3455@gmail.com>
2022-12-28 00:46:31 -0300

    modified: Comments on Linecode.py

--- LineCode.py
+++ LineCode.py
@@ -1,23 +1,33 @@
+# Authors:
+# Andrea Alexandra Sánez Oquendo
+# Fernanda Rocha Costa Neto
+
 class B8ZS:
     def __init__(self):
-        self.bits = []
+        self.bits = [] # original signal
         self.digitalSignal = []
 
+    
     def encode(self, bits):
+        # cleans auxiliar list
+        self.digitalSignal.clear()
+
         self.bits = bits
-        self.digitalSignal = []
         self.ami()
         self.violationBipolar()
         return self.digitalSignal
 
     def decode(self, signal):
         self.digitalSignal = signal
-        self.bits = []
+        self.bits.clear()
         self.undoViolation()
         self.undoAMI()
         
         return self.bits
 
+    ### AMI (Alternate Mark Inversion)
+    ### Parameters: None
+    ### Description: Synchronous clock encoding technique that uses bipolar pulses to represent logical 1.
     def ami(self):
         up = True
         for bit in self.bits:
@@ -30,11 +40,15 @@ class B8ZS:
             else:
                 self.digitalSignal.append(0)
 
+    ## Bipolar Violation of AMI
+    ## Parameters: None
+    ## Description: Replaces 8 consecutive 0's into a double violation
     def violationBipolar(self):
-        count = 0
+        
         violation = '00000000'
         signal = ''.join(str(i) for i in self.digitalSignal)
 
+        # it's easier to get the violation using strings
         while violation in signal:
             i = signal.index(violation)
             if signal[i - 1] == '1':
@@ -43,15 +57,15 @@ class B8ZS:
                 response = '00021012'
             
             signal = signal.replace(violation, response, 1)
-        
-        newSignal = []
 
-        letToNum = {'0': 0, '1': 1, '2': -1} 
-        for i in signal:
-            newSignal.append(letToNum[i])
+        letToNum = {'0': 0, '1': 1, '2': -1}
+        newSignal = [letToNum[i] for i in signal]
         
         self.digitalSignal = newSignal
-            
+
+    ## Undoes Bipolar Violation of AMI
+    ## Parameters: None
+    ## Description: Replaces violation by 8 consecutive 0's
     def undoViolation(self):
         signal = [2 if x==-1 else x for x in self.digitalSignal]
         signal = ''.join(str(i) for i in signal)
@@ -62,12 +76,9 @@ class B8ZS:
         
         signal = signal.replace(violation1, response)
         signal = signal.replace(violation2, response)
-        
-        newSignal = []
 
-        letToNum = {'0': 0, '1': 1, '2': -1} 
-        for i in signal:
-            newSignal.append(letToNum[i])
+        letToNum = {'0': 0, '1': 1, '2': -1}
+        newSignal = [letToNum[i] for i in signal]
         
         self.digitalSignal = newSignal
 
